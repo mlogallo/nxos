6c294422ef6f83f9f75f1aaa75120c566b68c065
mlogallo <85250289+mlogallo@users.noreply.github.com>
2021-12-07 03:43:10 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -24,34 +24,4 @@ resource "mso_tenant" "terraform_tenant" {
     description  = "This tenant is created by terraform"
 }
 
-# Define an MSO Tenant Resource.
-data "mso_tenant" "tenant_obj" {
-    name         = var.tenant
-    display_name = var.tenant
-}
 
-# Define an MSO Schema Resource.
-resource "mso_schema" "schema_obj" {
-    template_name = "Template1"
-    name          = var.schema
-    tenant_id     = data.mso_tenant.tenant_obj.id
-}
-
-# Define an MSO Schema VRF Resource.
-resource "mso_schema_template_vrf" "vrf_obj" {
-    schema_id     = mso_schema.schema_obj.id
-    template      = mso_schema.schema_obj.template_name
-    name          = var.vrf
-    display_name  = var.vrf
-}
-
-# Define an MSO Schema BD Resource.
-resource "mso_schema_template_bd" "bd_obj" {
-    schema_id              = mso_schema.schema_obj.id
-    template_name          = mso_schema.schema_obj.template_name
-    name                   = var.bd
-    display_name           = var.bd
-    vrf_name               = mso_schema_template_vrf.vrf_obj.name
-    layer2_unknown_unicast = "proxy"
-    layer2_stretch         = true
-}
